4ca56d0886527b39aaba7409cc25f08e094724a6
zenanikz <zenanikz@gmail.com>
2026-06-03 01:40:18 +0200

    Day 5 - Task 2 - Highest Score

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,6 @@
-student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+student_scores = [8, 65, 89, 86, 55, 91, 64, 89]
+
+#PAUSE 1 - Max without using the built-in function max()
+
+student_scores.sort()
+print(student_scores[-1])
