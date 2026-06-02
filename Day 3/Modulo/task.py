3922c37e1625e857724781ec21ac3bc9bafe0112
zenanikz <zenanikz@gmail.com>
2026-06-02 17:32:22 +0200

    Day 3 - Task 2 - Modulo and Conditionals

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,11 @@
+#PAUSE 1 - What is 10 % 3?
+
+#the code will output 1
+
+
+#PAUSE 2 - Check Odd or Even
+user_number = int(input("Please enter a random number\n"))
+if (user_number%2 == 0):
+    print(f"{user_number} is an even number.")
+else:
+    print(f"{user_number} is an odd number.")
