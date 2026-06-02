2cd546c778b90d200273690cca94c4265083ecf2
zenanikz <zenanikz@gmail.com>
2026-06-02 16:27:45 +0200

    Day 1 - Task 2 - String Manipulation with additional levels

--- Day 1/task/solution.py
+++ Day 1/task/solution.py
@@ -1,2 +1,17 @@
-# Write your code below this line 👇
-print("Hello world!")
+# 1. New Line Escape Sequence
+
+# Learn to use `\n` to add a new line to strings.
+
+print("Hello")
+print("Word")
+
+# is the same as:
+
+print("Hello\nWorld")
+
+# 2. String Concatenation
+# Use the plus symbol `+` to concatenate different strings together
+
+print("My name is" + " " + "Angela")
+
+# will output: "My name is Angela"
--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,24 @@
 # Write your code below this line 👇
-print("Hello World!")
+#PAUSE 1
+# basic
+print("Hello World\n")
+print("Hello World\n")
+print("Hello World\n")
+
+#advanced
+for x in range(1,4):
+    print("Hello world!")
+
+#PAUSE 2
+
+#basic
+#level 1
+print("Hello Zenani")
+
+# #level 2
+name = "Zenani"
+print("Hello", name)
+
+#level 3
+print(f"Hello {name}")
+
