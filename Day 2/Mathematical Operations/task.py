c930fecf90e81b80e428e9f79ca5c0f4feda0a56
zenanikz <zenanikz@gmail.com>
2026-06-02 17:15:18 +0200

    Day 2 - Task 3 - Mathematical Operations

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,13 @@
-print("My age: " + str(12))
+#PAUSE 1. What is the output of this code?
+'''
+print(3 * 3 + 3 / 3 - 3)
+'''
+
+#code outputs 7.0
+
+#PAUSE 2. Change the code so it outputs 3?
+'''
+print(3 * 3 + 3 / 3 - 3)
+'''
+
+print(int(((3 * (3 + 3)) / 3) - 3))
