0f703621fb1140c51fdb3bea87fc1431186f6348
zenanikz <zenanikz@gmail.com>
2026-06-02 16:56:42 +0200

    Day 1 - Task 6 - Band Name Generator

--- Day 1/task/solution.py
+++ Day 1/task/solution.py
@@ -1,11 +1,4 @@
-name = "Jack"
-print(name)
-
-name = "Angela"
-print(name)
-
-# print(len(input("What is your name?")))
-
-username = input("What is your name?")
-length = len(username)
-print(length)
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,8 +1,4 @@
-#1. Check the length of the user input
-name = input("What is your name?\n")
-print(len(name))
-
-#PAUSE 2. Split everything into variables.
-name = input("What is your name?\n")
-name_len = len(name)
-print(name_len)
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print(f"Your band name could be {city_name} {pet_name}")
