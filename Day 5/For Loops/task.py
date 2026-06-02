c0b5310c4e99338ab2206a3a1b44e0a483576b53
zenanikz <zenanikz@gmail.com>
2026-06-03 01:22:36 +0200

    Day 5 - Task 1 - For Loops

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,21 @@
 fruits = ["Apple", "Peach", "Pear"]
+
+#PAUSE 1 - Be a Computer
+'''
+Predict what will be printed from the code below:
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+'''
+
+#I think the code will print out something like this
+'''
+Apple
+Apple pie
+Peach
+Peach pie
+Pear
+Pear pie
+'''
