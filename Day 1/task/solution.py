9fbfa89e9348ad736a3e2b221cfed441a84e053f
zenanikz <zenanikz@gmail.com>
2026-06-02 16:41:17 +0200

    Day 1 - Task 3 - Inputs

--- Day 1/task/solution.py
+++ Day 1/task/solution.py
@@ -1,17 +1,2 @@
-# 1. New Line Escape Sequence
-
-# Learn to use `\n` to add a new line to strings.
-
-print("Hello")
-print("Word")
-
-# is the same as:
-
-print("Hello\nWorld")
-
-# 2. String Concatenation
-# Use the plus symbol `+` to concatenate different strings together
-
-print("My name is" + " " + "Angela")
-
-# will output: "My name is Angela"
+# This line of code will take an input using the input() function
+print("Hello " + input("What is your name?") + "!")
--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,24 +1,2 @@
-# Write your code below this line 👇
-#PAUSE 1
-# basic
-print("Hello World\n")
-print("Hello World\n")
-print("Hello World\n")
-
-#advanced
-for x in range(1,4):
-    print("Hello world!")
-
-#PAUSE 2
-
-#basic
-#level 1
-print("Hello Zenani")
-
-# #level 2
-name = "Zenani"
-print("Hello", name)
-
-#level 3
-print(f"Hello {name}")
-
+name = input("Please enter your name:\n")
+print(f"Hello {name}!")
