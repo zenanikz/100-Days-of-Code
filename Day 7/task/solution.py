ac0e5bfefbe1486bbe07e20118d4205041e8c2dd
zenanikz <zenanikz@gmail.com>
2026-06-03 04:48:03 +0200

    Day 7 - Task 5 - Hangman Step 5

--- Day 7/task/solution.py
+++ Day 7/task/solution.py
@@ -1,63 +1,11 @@
 import random
-stages = [r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-word_list = ["aardvark", "baboon", "camel"]
+
+from hangman_words import word_list
+from hangman_art import stages, logo
 
 lives = 6
+
+print(logo)
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -66,13 +14,18 @@ placeholder = ""
 word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
-print(placeholder)
+print("Word to guess: " + placeholder)
 
 game_over = False
 correct_letters = []
 
 while not game_over:
+
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
     guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters:
+        print(f"You've already guessed {guess}")
 
     display = ""
 
@@ -85,16 +38,19 @@ while not game_over:
         else:
             display += "_"
 
-    print(display)
+    print("Word to guess: " + display)
 
     if guess not in chosen_word:
         lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
         if lives == 0:
             game_over = True
-            print("You lose.")
+
+            print(f"***********************IT WAS {chosen_word}! YOU LOSE**********************")
 
     if "_" not in display:
         game_over = True
-        print("You win.")
+        print("****************************YOU WIN****************************")
 
     print(stages[lives])
