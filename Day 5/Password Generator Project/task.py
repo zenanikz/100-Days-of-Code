ddf7eabe58f266ac0bc952b8e42a678ad33dcd1b
zenanikz <zenanikz@gmail.com>
2026-06-03 02:08:08 +0200

    Day 5 - Task 4 - Password Generator Project

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -8,3 +10,41 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
+#Easy Version
+password = []
+created_password = ""
+
+for x in range(0, nr_letters):
+    password.append(random.choice(letters))
+
+for y in range(0, nr_numbers):
+    password.append(random.choice(numbers))
+
+for z in range(0, nr_symbols):
+    password.append(random.choice(symbols))
+
+for xyz in password:
+    created_password += xyz
+
+print(created_password)
+
+
+#Hard Version - Randomising the order of the password
+password = []
+created_password = ""
+
+for x in range(0, nr_letters):
+    password.append(random.choice(letters))
+
+for y in range(0, nr_numbers):
+    password.append(random.choice(numbers))
+
+for z in range(0, nr_symbols):
+    password.append(random.choice(symbols))
+
+random.shuffle(password)
+
+for xyz in password:
+    created_password += xyz
+
+print(created_password)
