f788556eca4b21d9091d460764332955d99baa8f
zenanikz <zenanikz@gmail.com>
2026-06-09 16:06:24 +0200

    Day 10 - Task 3 - Docstrings

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,7 @@
 def format_name(f_name, l_name):
+    """
+        Formatting names
+    """
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
