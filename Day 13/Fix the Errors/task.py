7034aab500132b2a9a391a8013d778462357dc37
zenanikz <zenanikz@gmail.com>
2026-06-18 14:13:25 +0200

    Day 13 - Task 4 - Fix The Errors

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,15 @@
+# PAUSE 1
+# Fix the bug so that the print statement displays the correct value of age in the output area.
+'''
 age = int(input("How old are you?"))
 if age > 18:
 print("You can drive at age {age}.")
+'''
+
+
+try:
+    age = int(input("How old are you?\n"))
+    if age > 18:
+        print(f"You can drive at age {age}.")
+except ValueError:
+    print("Your age isn't a valid number. Please fix that.")
