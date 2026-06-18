d56c7c8a26decb409e23519befa7b2d7ffd7fee5
zenanikz <zenanikz@gmail.com>
2026-06-18 14:02:27 +0200

    Day 13 - Task 3 - Play Computer

--- Day 13/Play Computer/task.py
+++ Day 13/Play Computer/task.py
@@ -1,6 +1,19 @@
+# PAUSE 1
+# Play computer and go through the code line by line as if you were executing the code to figure out why 1994 does not work as expected.
+# Then go ahead and fix the code.
+'''
 year = int(input("What's your year of birth?"))
 
 if year > 1980 and year < 1994:
+    print("You are a millennial.")
+elif year > 1994:
+    print("You are a Gen Z.")
+'''
+
+
+year = int(input("What's your year of birth?\n"))
+
+if year > 1980 and year <= 1994:
     print("You are a millennial.")
 elif year > 1994:
     print("You are a Gen Z.")
