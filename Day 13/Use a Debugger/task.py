36accd5c626885a950646473def174012c87a963
zenanikz <zenanikz@gmail.com>
2026-06-18 16:15:03 +0200

    Day 13 - Task 6 - Use a Debugger

--- Day 13/Use a Debugger/task.py
+++ Day 13/Use a Debugger/task.py
@@ -1,7 +1,9 @@
 import random
 import maths
 
-
+# PAUSE 1
+# Use the PyCharm debugger to figure out what is the issue in the starting code and fix it.
+'''
 def mutate(a_list):
     b_list = []
     new_item = 0
@@ -10,6 +12,20 @@ def mutate(a_list):
         new_item += random.randint(1, 3)
         new_item = maths.add(new_item, item)
     b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
+'''
+
+def mutate(a_list):
+    b_list = []
+    new_item = 0
+    for item in a_list:
+        new_item = item * 2
+        new_item += random.randint(1, 3)
+        new_item = maths.add(new_item, item)
+        b_list.append(new_item)
     print(b_list)
 
 
