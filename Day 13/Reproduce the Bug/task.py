0e95bc0118c1d9a5e9421a7b914b04ae69cf1283
zenanikz <zenanikz@gmail.com>
2026-06-18 13:56:38 +0200

    Day 13 - Task 2 - Reproduce the Bug

--- Day 13/Reproduce the Bug/task.py
+++ Day 13/Reproduce the Bug/task.py
@@ -1,4 +1,21 @@
 from random import randint
+'''
 dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
 dice_num = randint(1, 6)
+print(dice_images[dice_num])
+
+'''
+
+
+# PAUSE 1
+# Change the code above so that it always produces the occasional error.
+dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(6, 10)
+print(dice_images[dice_num])
+
+
+# PAUSE 2
+# Fix the code and remove the bug.
+dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 5)
 print(dice_images[dice_num])
